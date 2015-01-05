d082cdca583d463e4b7ca9bd1b1b1f937ac9de91
Niels Swimberghe <nielsswimberghe@gmail.com>
2015-01-06 00:25:15 +0100

    updated to the mvvm pattern. Actually tested, with simulation in kepserver

---
+++ ExamenVoorbereiding/DashboardVM.cs
@@ -0,0 +1,202 @@
+﻿using GalaSoft.MvvmLight.Command;
+using OPCLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace ExamenVoorbereiding.VM
+{
+    public class DashboardVM:INotifyPropertyChanged
+    {
+
+        private const string BASE_PATH = "examenvoorbereiding simulation.PLC.";
+        private OPCServerWrapper _server;
+        private OPCNodeWrapper _automatischWrapperNode;
+        private OPCNodeWrapper _vermogenWrapperNode;
+        private OPCNodeWrapper _isVoldoendeNode;
+        private OPCNodeWrapper _isWasMachineAanNode;
+        private OPCNodeWrapper _drukknopNode;
+        private DispatcherTimer _tmr;
+        private double _vermogenDrempel;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Point> Points { get; set; }
+
+        public bool Auto
+        {
+            get
+            {
+                return (bool)_automatischWrapperNode.Value;
+            }
+            set {
+                _automatischWrapperNode.Value = value;
+            }
+        }
+
+        public int GemetenVermogen
+        {
+            get
+            {
+                return (int)_vermogenWrapperNode.Value;
+            }
+            set
+            {
+                _vermogenWrapperNode.Value = value;
+            }
+        }
+
+        public bool IsVoldoende
+        {
+            get
+            {
+                return (bool)_isVoldoendeNode.Value;
+            }
+            set
+            {
+                _isVoldoendeNode.Value = value;
+            }
+        }
+
+        public double VermogenDrempel
+        {
+            get
+            {
+                return _vermogenDrempel;
+            }
+            set
+            {
+                _vermogenDrempel = value;
+                OnPropertyChanged("VermogenDrempel");
+                bool isVoldoende = IsVoldoende;
+                IsVoldoende = GemetenVermogen > VermogenDrempel;
+                if (isVoldoende != IsVoldoende)
+                    OnPropertyChanged("IsVoldoende");
+            }
+        }
+
+        public bool MachineDraait
+        {
+            get {
+                return (bool)_isWasMachineAanNode.Value;
+            }
+            set {
+                _isWasMachineAanNode.Value = value;
+            }
+        }
+
+        public bool Drukknop
+        {
+            get
+            {
+                return (bool)_drukknopNode.Value;
+            }
+            set
+            {
+                _drukknopNode.Value = value;
+            }
+        }
+
+        public DashboardVM()
+        {
+            Points = new ObservableCollection<Point>();
+            Init();
+        }
+
+        private void Init()
+        {
+            GetWrappers();
+            _tmr = new DispatcherTimer();
+            _tmr.Interval = new TimeSpan(0,0,1);
+            _tmr.Tick += _tmr_Tick;
+            SubscribeToWrappers();
+            _tmr.Start();
+        }
+
+        private void GetWrappers()
+        {
+            _server = OPCServerWrapper.GetOPCServerWrapper("Kepware.KEPServerEX.V5");
+            OPCServerWrapper.SelectedServer = _server;
+            var wrappers = _server.GetLeaves();
+            _automatischWrapperNode = wrappers.Single(wrapper => wrapper.ItemId == BASE_PATH + "Automatisch");
+            _vermogenWrapperNode = wrappers.Single(wrapper => wrapper.ItemId == BASE_PATH + "Vermogen");
+            _isVoldoendeNode = wrappers.Single(wrapper => wrapper.ItemId == BASE_PATH + "IsVoldoendeVermogen");
+            _isWasMachineAanNode = wrappers.Single(wrapper => wrapper.ItemId == BASE_PATH + "IsWasMachineAan");
+            _drukknopNode = wrappers.Single(wrapper => wrapper.ItemId == BASE_PATH + "Drukknop");
+        }
+
+        private void SubscribeToWrappers()
+        {
+            _automatischWrapperNode.ValueChanged += _automatischWrapperNode_ValueChanged;
+            _vermogenWrapperNode.ValueChanged += _vermogenWrapperNode_ValueChanged;
+            _isWasMachineAanNode.ValueChanged += _isWasMachineAanNode_ValueChanged;
+            _drukknopNode.ValueChanged += _drukknopNode_ValueChanged;
+
+            _automatischWrapperNode.ListenToUpdates = true;
+            _vermogenWrapperNode.ListenToUpdates = true;
+            _isWasMachineAanNode.ListenToUpdates = true;
+            _drukknopNode.ListenToUpdates = true;
+        }
+
+        void _automatischWrapperNode_ValueChanged(object sender, EventArgs e)
+        {
+            OnPropertyChanged("Auto");
+        }
+
+        void _vermogenWrapperNode_ValueChanged(object sender, EventArgs e)
+        {
+            OnPropertyChanged("GemetenVermogen");
+            bool isVoldoende = IsVoldoende;
+            IsVoldoende = GemetenVermogen > VermogenDrempel;
+            if(isVoldoende!=IsVoldoende)
+                OnPropertyChanged("IsVoldoende");
+        }
+
+        void _isWasMachineAanNode_ValueChanged(object sender, EventArgs e)
+        {
+            OnPropertyChanged("MachineDraait");
+        }
+
+        void _drukknopNode_ValueChanged(object sender, EventArgs e)
+        {
+            OnPropertyChanged("Drukknop");
+        }
+
+        public ICommand StartStopAction
+        {
+            get
+            {
+                return new RelayCommand(StartStop);
+            }
+        }
+
+        private void StartStop()
+        {
+            Drukknop = !Drukknop;
+        }
+
+        void _tmr_Tick(object sender, EventArgs e)
+        {
+            Points.Add(new Point(DateTime.Now.Ticks, GemetenVermogen));
+            if (Points.Count > 20)
+            {
+                Points.RemoveAt(0);
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
--- ExamenVoorbereiding/MainWindow.xaml.cs
+++ ExamenVoorbereiding/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using OPCLib.Models;
+﻿using ExamenVoorbereiding.VM;
+using OPCLib.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.DataVisualization.Charting;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,85 +25,9 @@ namespace ExamenVoorbereiding
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const string BASE_PATH = "PLC.";
-        private OPCServerWrapper _server;
-        private OPCNodeWrapper _automatischWrapperNode;
-        private OPCNodeWrapper _vermogenWrapperNode;
-        private OPCNodeWrapper _isVoldoendeNode;
-        private OPCNodeWrapper _isWasMachineAanNode;
-        private OPCNodeWrapper _drukknopNode;
-        private DispatcherTimer _tmr;
         public MainWindow()
         {
             InitializeComponent();
-            Init();
-            this.Loaded += MainWindow_Loaded;
-        }
-
-        private void Init()
-        {
-            GetWrappers();
-            btnStartStop.MouseUp += btnStartStop_MouseUp;
-            _tmr = new DispatcherTimer();
-            _tmr.Interval = new TimeSpan(500);
-            _tmr.Tick += _tmr_Tick;
-        }
-
-        private void GetWrappers()
-        {
-            _server = OPCServerWrapper.GetOPCServerWrapper("Kepware.KEPServerEX.V5");
-            OPCServerWrapper.SelectedServer = _server;
-            var wrappers = _server.GetOPCNodeWrappers();
-            _automatischWrapperNode = wrappers.Single(wrapper => wrapper.ItemId == BASE_PATH + "Automatisch");
-            _vermogenWrapperNode = wrappers.Single(wrapper => wrapper.ItemId == BASE_PATH + "Vermogen");
-            _isVoldoendeNode = wrappers.Single(wrapper => wrapper.ItemId == BASE_PATH + "IsVoldoendeVermogen");
-            _isWasMachineAanNode = wrappers.Single(wrapper => wrapper.ItemId == BASE_PATH + "IsWasMachineAan");
-            _drukknopNode = wrappers.Single(wrapper => wrapper.ItemId == BASE_PATH + "Drukknop");
-        }
-
-        private void SubscribeToWrappers()
-        {
-            _automatischWrapperNode.ValueChanged += _automatischWrapperNode_ValueChanged;
-            _vermogenWrapperNode.ValueChanged += _vermogenWrapperNode_ValueChanged;
-            _isWasMachineAanNode.ValueChanged += _isWasMachineAanNode_ValueChanged;
-        }
-
-        void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            SubscribeToWrappers();
-            _tmr.Start();
-        }
-
-        void _automatischWrapperNode_ValueChanged(object sender, EventArgs e)
-        {
-            chkAuto.IsOn = (bool) _automatischWrapperNode.Value;
-        }
-
-        void _vermogenWrapperNode_ValueChanged(object sender, EventArgs e)
-        {
-            double vermogen = (double) _vermogenWrapperNode.Value;
-            txtGemetenVermogen.Text = vermogen.ToString();
-            _isVoldoendeNode.Value = vermogen > txtVermogenDrempel.Value;
-        }
-
-        void _isWasMachineAanNode_ValueChanged(object sender, EventArgs e)
-        {
-            chkMachineDraait.IsOn = (bool) _isWasMachineAanNode.Value;
-        }
-
-        void btnStartStop_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            //throw new NotImplementedException();
-            _drukknopNode.Value = true;
-        }
-
-        void _tmr_Tick(object sender, EventArgs e)
-        {
-            line.Points.Add(new Point(DateTime.Now.Second,(double)_vermogenWrapperNode.Value));
-            if (line.Points.Count > 20)
-            {
-                line.Points.RemoveAt(0);
-            }
         }
     }
 }
